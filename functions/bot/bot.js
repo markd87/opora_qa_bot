f884c323380e08cad02aa221cd6b455954a915d2
Mark Danovich <mark.danovich@gmail.com>
2023-05-31 20:43:50 +0100

    try

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -50,14 +50,17 @@ bot.action("topic1", async (ctx) => {
 });
 
 bot.action("q1", async (ctx) => {
-  await ctx.replyWithHTML(
-    "<b>Do you have parents in the UK?</b>",
-    Markup.keyboard([
-      [Markup.button.callback("Yes", "q1_1")],
-      [Markup.button.callback("No - I don't have family in the UK", "q1_2")],
-    ])
+  await ctx.replyWithMarkdownV2(
+    "*Do you have parents in the UK?*",
+    Extra.webPreview(true)
+      .markup((m) =>
+        m.keyboard([
+          [m.button.callback("Yes", "q1_1")],
+          [m.button.callback("No - I don't have family in the UK", "q1_2")],
+        ])
+      )
+      .oneTime()
       .resize()
-      .extra()
   );
   ctx.answerCbQuery();
 });
