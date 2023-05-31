28e86fe6089c743593b3c2e7b94c4837343e27f8
Mark Danovich <mark.danovich@gmail.com>
2023-05-31 21:14:40 +0100

    try again

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -62,10 +62,12 @@ bot.action("q1", async (ctx) => {
 });
 
 bot.action("q1_1", async (ctx) => {
-  await ctx.editMessageReplyMarkup([
-    [Markup.button.callback("✅ Yes", "q1_1")],
-    [Markup.button.callback("No - I don't have family in the UK", "q1_2")],
-  ]);
+  await ctx.editMessageReplyMarkup({
+    inline_keyboard: [
+      [Markup.button.callback("✅ Yes", "q1_1")],
+      [Markup.button.callback("No - I don't have family in the UK", "q1_2")],
+    ],
+  });
 });
 
 exports.handler = async (event) => {
