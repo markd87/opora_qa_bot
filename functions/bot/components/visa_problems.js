cd6f45496672799e1fc279651e2375109074e258
Mark Danovich <mark.danovich@gmail.com>
2023-06-15 22:39:30 +0100

    last try

--- functions/bot/components/visa_problems.js
+++ functions/bot/components/visa_problems.js
@@ -255,10 +255,10 @@ exports.visa_problems = (bot) => {
       ])
     );
 
-    // await ctx.sendPhoto(
-    //   ctx.chat.id,
-    //   "https://thriving-frangollo-33fd04.netlify.app/assets/status_says.jpeg"
-    // );
+    await ctx.telegram.sendPhoto(
+      ctx.chat.id,
+      "https://thriving-frangollo-33fd04.netlify.app/assets/status_says.jpeg"
+    );
 
     ctx.answerCbQuery();
   });
