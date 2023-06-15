7662e7c0eef2b6ccbddec33d49bd794a31f309fe
Mark Danovich <mark.danovich@gmail.com>
2023-06-15 20:55:01 +0100

    try again

--- functions/bot/components/visa_problems.js
+++ functions/bot/components/visa_problems.js
@@ -1,5 +1,5 @@
 const { Markup } = require("telegraf");
-const Extra = require("telegraf/extra");
+const { Extra } = require("telegraf");
 
 exports.visa_problems = (bot) => {
   bot.action("problem_visa", async (ctx) => {
