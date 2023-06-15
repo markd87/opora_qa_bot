5473f170efd73a9e9fb3541527561b7e1110ced1
Mark Danovich <mark.danovich@gmail.com>
2023-06-15 22:17:54 +0100

    try again

--- functions/bot/components/visa_problems.js
+++ functions/bot/components/visa_problems.js
@@ -255,10 +255,10 @@ exports.visa_problems = (bot) => {
       ])
     );
 
-    await ctx.telegram.sendPhoto(
-      ctx.chat.id,
-      "https://thriving-frangollo-33fd04.netlify.app/assets/c_Screenshot 2023-06-10 at 17.28.07.png"
-    );
+    // await ctx.telegram.sendPhoto(
+    //   ctx.chat.id,
+    //   "https://thriving-frangollo-33fd04.netlify.app/assets/c_Screenshot 2023-06-10 at 17.28.07.png"
+    // );
     ctx.answerCbQuery();
   });
 
