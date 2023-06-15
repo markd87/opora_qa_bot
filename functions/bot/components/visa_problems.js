9ac161aae96060da0b75e74e7269bd1c0b799209
Mark Danovich <mark.danovich@gmail.com>
2023-06-15 22:11:56 +0100

    revert

--- functions/bot/components/visa_problems.js
+++ functions/bot/components/visa_problems.js
@@ -259,7 +259,7 @@ exports.visa_problems = (bot) => {
     const imageUrl =
       "https://thriving-frangollo-33fd04.netlify.app/assets/c_Screenshot 2023-06-10 at 17.28.07.png";
 
-    await ctx.telegram.sendPhoto(ctx.chat.id, imageUrl);
+    // await ctx.telegram.sendPhoto(ctx.chat.id, imageUrl);
     ctx.answerCbQuery();
   });
 
