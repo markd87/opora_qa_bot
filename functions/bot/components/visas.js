08aafca67700d1baf44a34698c144271acd8f3ef
Mark Danovich <mark.danovich@gmail.com>
2023-06-25 23:35:35 +0100

    clear

--- functions/bot/components/visas.js
+++ functions/bot/components/visas.js
@@ -2,6 +2,15 @@ const { Markup, Telegraf } = require("telegraf");
 
 exports.visas = (bot) => {
   bot.action("visas", async (ctx) => {
+    // clear keyboard
+    try {
+      await ctx.editMessageReplyMarkup({
+        reply_markup: { remove_keyboard: true },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
     await ctx.replyWithHTML(
       `<b>Виберіть тему:</b>`,
       Markup.inlineKeyboard([
