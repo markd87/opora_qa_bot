de786918955807b2ee4f863e75a5a2325c7d172a
Mark Danovich <mark.danovich@gmail.com>
2023-06-30 22:18:22 +0100

    changed ok

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -73,7 +73,7 @@ bot.action("general", async (ctx) => {
 
 bot.action("OK", async (ctx) => {
   await ctx.replyWithHTML(
-    `✅ Дякуємо, що скористалися нашим асистентом!`,
+    `✅ Дякуємо, що скористалися нашим асистентом! Щоб перейти до візового чату, натисніть https://t.me/oporaukchat`,
     Markup.removeKeyboard()
   );
   ctx.answerCbQuery();
